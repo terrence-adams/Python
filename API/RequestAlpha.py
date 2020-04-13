a5f1b79af33f3d6fb972128c3b875a8ff75781c2
Terrence Adams <terrence.adams@gmail.com>
2020-04-13 17:06:51 -0500

    updated credit services test run

--- API/RequestAlpha.py
+++ API/RequestAlpha.py
@@ -1,5 +1,5 @@
-from BaseClass import ApiRequest
-from BaseClass import ApiResponse
+from Python.API.BaseClass import ApiRequest
+from Python.API.BaseClass import ApiResponse
 import requests
 import json
 import io
@@ -12,11 +12,10 @@ class RequestAlpha:
     json_post_body = {}
     preview_credit_url = 'https://preview.sync1creditservices.com/api/Credit'
     preview_login_url = 'https://preview.sync1creditservices.com/api/Login'
-    login_request_session =  requests.session()
+    login_request_session = requests.session()
     token = ''
     header = {'Content-type': 'application/json-patch+json', 'accept': 'text/plain'}
-    body = json= {'username': 'sync1@cuanswers.com', 'password': 'Testsync1'}
-
+    body = json = {'username': 'sync1@cuanswers.com', 'password': 'Sync1test2'}
 
     def __init__(self):
         pass
@@ -25,19 +24,16 @@ class RequestAlpha:
   #  def write_to_string(self):
    #     a = io.StringIO()
     #    with open(a, 'w') as my_string:
-     #       my_string.write("Hello my world!")
-      #      print(a)
+     # my_string.write("Hello my world!")
+      # print(a)
 
     # loads from json file, returns a string which is then coverted into a python dict
     def import_json(self):
-        #self.file_name = file_name
-        with open('C:\Repo\Python\API\JsonFiles\sync1_tu_sample_request.json', 'r') as js:
+        # self.file_name = file_name
+        with open('./JsonFiles/sync1_tu_sample_request.json', 'r') as js:
             json_string = js
 
             self.json_post_body = json.load(json_string)
-            #print(json_string)
-
-        #print(self.json_post_body.items())
 
     def log_in(self):
 
@@ -45,16 +41,16 @@ class RequestAlpha:
             self.preview_login_url, headers=self.header, json=self.body)
         login_dict = json.loads(resp.text)
         self.token = 'bearer ' + login_dict['token']['access_token']
-        #print(self.token)
         return self.token
 
     def post_credit_to_credit_services(self):
         my_token = self.log_in()
         self.import_json()
-        self.header.update({'authorization' : self.token})
-        resp = requests.post(self.preview_credit_url, headers= self.header, json= self.json_post_body)
-        print(resp.status_code)
-        self.write_resp_to_file(resp.json())
+        self.header.update({'authorization': self.token})
+        resp = requests.post(self.preview_credit_url, headers=self.header, json=self.json_post_body)
+        print('Response status: ', resp.status_code)
+        # self.write_resp_to_file(resp.json()) enable when testing locally to write resp to file
+        self.print_response(resp.text)
         time.sleep(2)
 
     def create_multiple_submissions(self, i=1):
@@ -69,13 +65,18 @@ class RequestAlpha:
     """
 
     def write_resp_to_file(self, response_body):
-        file_name = 'sync1_tu_sample_request.json' + str(time.process_time()).replace(".", "") + '.json'
+        file_name = 'sync1_tu_sample_response' + str(time.process_time()).replace(".", "") + '.json'
         with open(file_name, 'w') as file1:
             # file1.write(json.dumps(response_body, indent=4, sort_keys=True))
             file1.write(json.dumps(response_body, indent=4, sort_keys=True))
 
+    def print_response(self, response_body):
+            print(response_body)
 
-    #looping through and exploring the xml for symitar
+
+
+
+    # looping through and exploring the xml for symitar
     def import_xml(self):
         tree = ET.parse('XMLFiles/memberlookup.xml')
         root = tree.getroot()
@@ -107,10 +108,7 @@ class RequestAlpha:
                                 print(j.text)
 
 
-
-
-
 if __name__ == '__main__':
     reqA = RequestAlpha()
-    #reqA.create_multiple_submissions(3)
-    reqA.import_xml()
+    reqA.create_multiple_submissions(2)
+    reqA.post_credit_to_credit_services()
